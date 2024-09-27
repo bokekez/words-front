c7d7ae69f813e6704d9ecb68540167b2420c0676
bokekez <bokekez@gmail.com>
2024-09-27 23:49:03 +0200

    added model info

--- src/components/Model.js
+++ src/components/Model.js
@@ -1,10 +1,12 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { getActiveModel, switchModel } from '../apiService/modelApi';
 import '../componentStyles/Model.css';
 import { ModelContext } from '../context/modelContext';
+import questionMark from '../resources/question.png'
 
 const Model = () => {
   const { model, setModel } = useContext(ModelContext);
+  const[showInfo, setShowInfo] = useState(false);
 
   useEffect(() => {
     const getModel = async () => {
@@ -23,13 +25,25 @@ const Model = () => {
     setModel(value)
   }
 
+  const handleInfo = () => {
+    setShowInfo(showInfo === false ? true : false)
+  }
+
   return (
     <div className="model">
       <p>Active Model:</p>
-      <div>
+      <div className="model-container">
         <button className={`${model === 'basic' ? 'active' : ''}`} onClick={() => handleModelChange('basic')}>Basic</button>
         <button className={`${model === 'transitive' ? 'active' : ''}`} onClick={() => handleModelChange('transitive')}>Transitive</button>
+        <img src={questionMark} alt="Question Mark" className="model-question" onClick={handleInfo}/>
       </div>
+      {showInfo && (
+      <div onClick={handleInfo} className="model-info">
+        <p>Basic model - Words and synonyms are symetrical, if A is a synonym of B and B is a synonym of C, then A, B and C are all syonoyms</p>
+        <p>Transitive - Words and synonyms are not symetrical, if A is a synonym of B and B is a synonym of C, then A and C are transitive synoyms</p>
+        <button>X</button>
+      </div>
+      )}
     </div>
   )
 }
