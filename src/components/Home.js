9b755ab53f1aa2d511ac4db1dc4ed393e65a91d3
bokekez <bokekez@gmail.com>
2024-10-12 18:21:05 +0200

    removed references

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import reeinvent_logo from '../resources/reeinvent_logo.png';
+import words_logo from '../resources/words_logo.png';
 import '../componentStyles/Home.css';
 
 const Home = () => {
   return (
     <div className="home-container">
-      <img src={reeinvent_logo} alt="Logo" className="logo" />
-      <h1 className="header-text">Reeinvent Words</h1>
+      <img src={words_logo} alt="Logo" className="logo" />
+      <h1 className="header-text">Words and synoyms</h1>
     </div>
   );
 };
